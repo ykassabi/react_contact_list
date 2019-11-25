2707013af6b80979ccc8c44b0730b9d1aeb7a158
Youssef M Kassabi <youssef@devmyk.com>
2019-11-25 20:31:22 +0100

    implimentation of API

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, { Component } from 'react';
 import ContactList from './ContactList';
+import * as ContactAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state= {
-    contacts: [{
-      "id": "karen",
-      "name": "Karen Isgrigg",
-      "handle": "karen_isgrigg",
-      "avatarURL": "http://localhost:5001/karen.jpg"
-    },
-    {
-      "id": "richard",
-      "name": "Richard Kalehoff",
-      "handle": "richardkalehoff",
-      "avatarURL": "http://localhost:5001/richard.jpg"
-    },
-    {
-      "id": "tyler",
-      "name": "Tyler McGinnis",
-      "handle": "tylermcginnis",
-      "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-    ]
+    contacts: []  
   }
+
+
   removeContact = (contact) => {
     this.setState((currentState) => ({
         contacts: currentState.contacts.filter((c) => {
-                              return c.id !== contact.id})
-  }))}
+                              return c.id !== contact.id
+        })
+    }))
+    ContactAPI.remove(contact)
+  }
 
-
+componentDidMount(){
+  ContactAPI.getAll()
+  .then((contacts)=>{
+    this.setState(()=>({ 
+      contacts
+    }))})
+}
 
   render() {
     return (
